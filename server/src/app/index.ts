49ad4f657e816a09cc50dfb0070ceacbe562eeed
sychova <anastasiya.sychova@gmail.com>
2024-03-19 21:41:59 +0300

    feature: dependencies

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello");
+import express, { Request, Response } from "express";
+import cors from "cors";
+// import dotenv from "dotenv"
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
