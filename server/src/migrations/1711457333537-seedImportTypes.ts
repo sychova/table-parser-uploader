06985182a12877bbe7406f5a06f8773c99b26da5
sychova <anastasiya.sychova@gmail.com>
2024-03-27 13:32:47 +0300

    fix: fix seeding migrations

--- server/src/migrations/1711457333537-seedImportTypes.ts
+++ server/src/migrations/1711457333537-seedImportTypes.ts
@@ -3,11 +3,37 @@ import {
   ImportTypeDimensionCoordinatesSeed,
   ImportTypeActionsDimensionCoordinatesSeed,
 } from "./seeds/importTypeDimensionCoordinates.seed";
-import { AppDataSource } from "../data-source";
 import { ImportType, ImportTypeActions } from "../app/entities";
 
 export class SeedImportTypes1711457333537 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into(ImportType)
+      .values([{ name: ImportTypeDimensionCoordinatesSeed }])
+      .execute();
+
+    const importTypeId = (
+      await queryRunner.manager
+        .createQueryBuilder()
+        .select("id")
+        .from(ImportType, "import_type")
+        .where("import_type.name = :name", {
+          name: ImportTypeDimensionCoordinatesSeed,
+        })
+        .getRawOne()
+    ).id;
+
+    await ImportTypeActionsDimensionCoordinatesSeed.forEach(async (elem) => {
+      await queryRunner.manager
+        .createQueryBuilder()
+        .insert()
+        .into(ImportTypeActions)
+        .values([{ ...elem, importType: importTypeId }])
+        .execute();
+    });
+  }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
